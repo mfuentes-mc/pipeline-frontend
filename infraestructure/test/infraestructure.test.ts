6abc9287f04ead1b5ffd096c0bf82f49823f7eec
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2022-11-13 18:09:34 -0600

    refactor: update structur of project

--- infraestructure/test/infraestructure.test.ts
+++ infraestructure/test/infraestructure.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as PipelineFrontend from '../lib/pipeline-frontend-stack';
+// import * as Infraestructure from '../lib/infraestructure-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/pipeline-frontend-stack.ts
+// example resource in lib/infraestructure-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new PipelineFrontend.PipelineFrontendStack(app, 'MyTestStack');
+//   const stack = new Infraestructure.InfraestructureStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
