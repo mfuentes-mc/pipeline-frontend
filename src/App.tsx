2e5ebad535022cb96aecb6d3b154ea40faf4e543
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2022-11-13 20:00:12 -0600

    refactor: update logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import futuraTvLogo from './assets/futuratv.png'
+import futura923Logo from './assets/futura923.png'
 import './App.css'
 
 function App() {
@@ -8,25 +9,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      <a href="https://www.facebook.com/profile.php?id=100075859906235" target="_blank">
+          <img src={futuraTvLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>CUPONERA FUTURA TV</h1>
     </div>
   )
 }
