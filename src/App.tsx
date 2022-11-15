f57f23925ee60a33e47e7ad5bad835ac10217954
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2022-11-14 18:28:47 -0600

    refactor: update structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import futuraTvLogo from './assets/futuratv.png'
-import futura923Logo from './assets/futura923.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-      <a href="https://www.facebook.com/profile.php?id=100075859906235" target="_blank">
-          <img src={futuraTvLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>CUPONERA FUTURA TV</h1>
-    </div>
+    <>
+      <h1>Bebidas</h1>
+    </>
   )
 }
 
